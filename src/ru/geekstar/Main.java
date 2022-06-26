a7b4110d59008bc9a9aa734680d000356ee56316
Kamila Kalugina <kamilyok@yandex.ru>
2022-06-26 12:07:02 +0400

    first commit

---
+++ src/ru/geekstar/Main.java
@@ -0,0 +1,10 @@
+package ru.geekstar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // main - ветка хранит всегда стабильную версию кода
+
+        System.out.println("Hello world!");
+    }
+}
